14dc821b71d02df6eebf944d154738ca96f598e5
nezl0i <nezl0in@gmail.com>
2021-01-11 23:05:07 +0300

    Задание_1

---
+++ task_1.py
@@ -0,0 +1,17 @@
+a = 4
+b = 7.2
+c = 'Task_1'
+
+print(a)
+print(b)
+print(c)
+
+number_one = int(input('Введите целое число: '))
+print(number_one)
+
+number_two = float(input('Введите дробное число: '))
+print(number_two)
+
+user_name = input('Как Вас зовут: ')
+user_age = int(input('Сколько Вам лет: '))
+print(f'О,{user_name}, в {user_age} год(а) Вы отлично выглядите!!')
